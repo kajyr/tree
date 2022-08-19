ab3f20ad3cb245e933d3ada4a9fd8eca2757f4d7
Carlo <carlo.panzi@gmail.com>
2022-08-19 21:00:56 +0200

    fix: Correct placeholder image

--- frontend/src/pages/dashboard/person-card.tsx
+++ frontend/src/pages/dashboard/person-card.tsx
@@ -2,17 +2,15 @@ import { PersonFromMongo } from 'types';
 
 import React, { FC } from 'react';
 
-import { Badge, Button, Card, Group, Image, Text } from '@mantine/core';
+import { Button, Card, Group, Image, Text } from '@mantine/core';
+
+import placeholderMan from './placeholder-male.png';
 
 const PersonCard: FC<{ data: PersonFromMongo }> = ({ data }) => {
   return (
     <Card shadow="sm" p="lg" radius="md" withBorder>
       <Card.Section>
-        <Image
-          src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-          height={160}
-          alt="Norway"
-        />
+        <Image src={placeholderMan} height={160} alt="Placeholder photo" fit="contain" />
       </Card.Section>
 
       <Group mt="md" mb="xs">
